dc2aa7758cee63dd363be2de1e94ca0e575441dd
ayaan000 <82323240+ayaan000@users.noreply.github.com>
2023-10-30 14:15:43 -0400

    Game entity in Progress

--- src/entity/Game.java
+++ src/entity/Game.java
@@ -1,3 +1,61 @@
+import entity.*;
+
+import java.util.ArrayList;
+
 class Game {
+    private String currCategory;
+    private float timeLeft;
+    private Question currQuestion;
+
+    private ArrayList<Question> questionList;
+
+    Game(String currCategory, float timeLeft, Question currQuestion, ArrayList<Question> questionList)
+    {
+        this.currCategory = currCategory;
+        this.timeLeft = timeLeft;
+        this.currQuestion = currQuestion;
+        this.questionList = questionList;
+    }
+
+    void pauseGame()
+    {
+
+    }
+    void playGame()
+    {
+
+    }
+    void restartGame()
+    {
+
+    }
+    void returnToLobby()
+    {
+
+    }
+    void computePoints()
+    {
+
+    }
+
+    void getPlayerAns()
+    {
+
+    }
+
+    void checkCorrect()
+    {
+
+    }
+
+    void getQuestion()
+    {
+
+    }
+
+    void getAns()
+    {
+
+    }
 
 }
