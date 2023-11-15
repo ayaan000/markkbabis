c7cede9c574209aed94b9c29c9ef5117ccf45279
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-15 17:41:54 -0500

    add Question in the InputData

--- src/use_case/SelectAnswerInputData.java
+++ src/use_case/SelectAnswerInputData.java
@@ -1,8 +1,23 @@
 package use_case;
 
+import entity.Question;
+
 public class SelectAnswerInputData {
+    private Question question;
     private int answer;
-    public SelectAnswerInputData(int answer) {
+    public SelectAnswerInputData(Question question, int answer) {
+        this.answer = answer;
+    }
+    public Question getQuestion(Question question) {
+        return question;
+    }
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+    public int getAnswer(int answer) {
+        return answer;
+    }
+    public void setAnswer(int answer) {
         this.answer = answer;
     }
 }
