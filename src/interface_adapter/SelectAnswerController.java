cf36b3319804e85357288a4127f32205343b6fc2
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-15 17:25:09 -0500

    select answer controller

--- src/interface_adapter/SelectAnswerController.java
+++ src/interface_adapter/SelectAnswerController.java
@@ -1,17 +1,17 @@
-//package interface_adapter;
-//
-//import use_case.SelectAnswer;
-//import use_case.SelectAnswerInputBoundary;
-//import use_case.SelectAnswerInputData;
-//
-//public class SelectAnswerController {
-//    final SelectAnswerInputBoundary userSelectAnswerInteractor;
-//    public SelectAnswerController(SelectAnswerInputBoundary userSignupUseCaseInteractor) {
-//        this.userSelectAnswerInteractor = userSignupUseCaseInteractor;
-//    }
-//
-//    public void execute(int answer) {
-//        SelectAnswerInputData selectAnswerInputData = new SelectAnswerInputData(answer);
-//        userSelectAnswerInteractor.execute(selectAnswerInputData);
-//    }
-//}
+package interface_adapter;
+
+import use_case.SelectAnswer;
+import use_case.SelectAnswerInputBoundary;
+import use_case.SelectAnswerInputData;
+
+public class SelectAnswerController {
+    final SelectAnswerInputBoundary userSelectAnswerInteractor;
+    public SelectAnswerController(SelectAnswerInputBoundary userSignupUseCaseInteractor) {
+        this.userSelectAnswerInteractor = userSignupUseCaseInteractor;
+    }
+
+    public void execute(int answer) {
+        SelectAnswerInputData selectAnswerInputData = new SelectAnswerInputData(answer);
+        userSelectAnswerInteractor.execute(selectAnswerInputData);
+    }
+}
