6a2b3a70aa16bf6c2fbf244f7bff245cca31c8df
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-15 17:20:58 -0500

    create use_case and interface_adapter package

---
+++ src/use_case/SelectAnswerInputBoundary.java
@@ -0,0 +1,5 @@
+package use_case;
+
+public interface SelectAnswerInputBoundary {
+    void execute(SelectAnswerInputData signupInputData);
+}
---
+++ src/use_case/SelectAnswerInputData.java
@@ -0,0 +1,8 @@
+package use_case;
+
+public class SelectAnswerInputData {
+    private int answer;
+    public SelectAnswerInputData(int answer) {
+        this.answer = answer;
+    }
+}
