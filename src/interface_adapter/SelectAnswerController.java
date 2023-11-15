3ff8dceb9250232525ea253926ae7d22eddb3ba9
ayaan000 <82323240+ayaan000@users.noreply.github.com>
2023-11-15 18:44:14 -0500

    Added SelectAnswerInputBoundary and SelectAnswerInputData to use case

---
+++ src/interface_adapter/SelectAnswerController.java
@@ -0,0 +1,13 @@
+package interface_adapter;
+
+import use_case.SelectAnswerInputBoundary;
+import use_case.SelectAnswerInputData;
+
+public class SelectAnswerController {
+
+    SelectAnswerInputBoundary selectAnswerInputBoundary;
+
+    public SelectAnswerController(){
+
+    }
+}
