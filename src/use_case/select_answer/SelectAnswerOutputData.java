8caebc4fd3a1563933e17e0297305aea36179478
Inkim1 <matthew.inkim@mail.utoronto.ca>
2023-11-19 15:08:54 -0500

    Adjusted the SelectAnswerOutputData input set

--- src/use_case/select_answer/SelectAnswerOutputData.java
+++ src/use_case/select_answer/SelectAnswerOutputData.java
@@ -4,15 +4,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 
 public class SelectAnswerOutputData {
-<<<<<<< HEAD
-
-    private String correctAnswer;
-    private Duration timeLeft;
-    private boolean correctness;
-
-    public SelectAnswerOutputData(String answer, Duration timeLeft, boolean correctness) {
-        this.correctAnswer = answer;
-=======
     private int playerPoints;
     private int compPoints;
     private ArrayList<String> questions;
@@ -26,13 +17,10 @@ public class SelectAnswerOutputData {
         this.compPoints = compPoints;
         this.questions = questions;
         this.answer = answer;
->>>>>>> 0f8f05a (Completed use case for selecting answer other than interactor)
         this.timeLeft = timeLeft;
         this.correctness = correctness;
     }
 
-<<<<<<< HEAD
-=======
     public int getPlayerPoints() {
         return playerPoints;
     }
@@ -48,7 +36,6 @@ public class SelectAnswerOutputData {
     public String getAnswer() {
         return answer;
     }
->>>>>>> 0f8f05a (Completed use case for selecting answer other than interactor)
 
     public Duration getTimeLeft() {
         return timeLeft;
@@ -57,10 +44,4 @@ public class SelectAnswerOutputData {
     public boolean getCorrectness() {
         return correctness;
     }
-<<<<<<< HEAD
-
-    public String getCorrectAnswer() {
-        return correctAnswer;}
-=======
->>>>>>> 0f8f05a (Completed use case for selecting answer other than interactor)
 }
