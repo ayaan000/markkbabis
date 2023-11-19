b626cd5afd2540f39bf178a5028e9d86c729900b
Inkim1 <matthew.inkim@mail.utoronto.ca>
2023-11-19 16:03:33 -0500

    Got through a good amount of GameStatsInteractor

--- src/use_case/game_stats/GameStatsInteractor.java
+++ src/use_case/game_stats/GameStatsInteractor.java
@@ -1,4 +1,27 @@
 package use_case.game_stats;
 
-public class GameStatsInteractor {
+import java.time.Duration;
+
+public class GameStatsInteractor implements GameStatsInputBoundary {
+    private GameStatsOutputBoundary userPresenter;
+
+    public GameStatsInteractor(GameStatsOutputBoundary gameStatsOutputBoundary) {
+        this.userPresenter = gameStatsOutputBoundary;
+    }
+
+    public void execute() {
+
+        // List of inputs to create gameStatsOutputData
+        String category = ;
+        String difficulty = ;
+        int numQuestions = ;
+        int points1 = ;
+        int points2 = ;
+        int numCorrectAnswers = ;
+        int numCorrectAnswers = ;
+        Duration timePlayed = ;
+
+        GameStatsOutputData gameStatsOutputData = new GameStatsOutputData();
+        userPresenter.prepareSuccessView(gameStatsOutputData);
+    }
 }
