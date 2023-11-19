4298d500ebc0d9b245899be341bfe41f4afc4eed
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-19 15:55:13 -0500

    CalculatePointOutputBoundary

--- src/use_case/calculate_point/CalculatePointOutputBoundary.java
+++ src/use_case/calculate_point/CalculatePointOutputBoundary.java
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-package use_case.calculate_point;
-
-public interface CalculatePointOutputBoundary {
-    void execute(CalculatePointOutputData calculatePointOutputData);
-=======
 package use_case.calculate_point;public class CalculatePointOutputBoundary {
->>>>>>> b21a1d0 (Implement of CalculatePointInputBoundary)
 }
