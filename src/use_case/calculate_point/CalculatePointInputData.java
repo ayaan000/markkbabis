3e372a898f7eff0f678e6037965a1dc10f545c96
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-19 14:58:59 -0500

    implement CalculatePointInputData

--- src/use_case/calculate_point/CalculatePointInputData.java
+++ src/use_case/calculate_point/CalculatePointInputData.java
@@ -1,2 +1,11 @@
-package use_case.calculate_point;public interface CalculatePointInputData {
+package use_case.calculate_point;
+
+public class CalculatePointInputData {
+    private boolean correctness;
+    public CalculatePointInputData(boolean correctness) {
+        this.correctness = correctness;
+    }
+    public boolean getCorrectness() {
+        return correctness;
+    }
 }
