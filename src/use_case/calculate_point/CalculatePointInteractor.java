37f2835477660582a2a1054d9512dd1c8b9f199d
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-19 15:48:08 -0500

    implement CalculatePointInteractor without DAO

--- src/use_case/calculate_point/CalculatePointInteractor.java
+++ src/use_case/calculate_point/CalculatePointInteractor.java
@@ -1,2 +1,27 @@
-package use_case.calculate_point;public class CalculatePointInteractor {
+package use_case.calculate_point;
+
+public class CalculatePointInteractor implements CalculatePointInputBoundary{
+    final CalculatePointDataAccessInterface dataAccessObject;
+    final CalculatePointOutputBoundary calculatePointPresenter;
+
+    public CalculatePointInteractor(CalculatePointDataAccessInterface dataAccessObject, CalculatePointOutputBoundary
+            calculatePointPresenter) {
+        this.dataAccessObject = dataAccessObject;
+        this.calculatePointPresenter = calculatePointPresenter;
+    }
+
+    @Override
+    public void execute(CalculatePointInputData calculatePointInputData) {
+        boolean correctness = false;
+        if (!(dataAccessObject.matchAnswer())) {
+            player.point += 0; //where to access player
+            CalculatePointOutputData calculatePointOutputData = new CalculatePointOutputData(false, player.point);
+            execute CalulatePointPresenter; //idea only because hasn't able to access DAO
+        }
+        else { //correctAnswer
+            player.point = player.point + 100 + round(10 * question.timeLeft); //algo
+            CalculatePointOutputData calculatePointOutputData = new CalculatePointOutputData(true, player.point);
+            execute CalulatePointPresenter; //idea only because hasn't able to access DAO
+        }
+    }
 }
