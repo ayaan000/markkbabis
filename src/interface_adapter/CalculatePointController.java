9730267e02cd9ae6316b95986562ba97486e9695
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-19 15:29:57 -0500

    implement CalculatePointController

---
+++ src/interface_adapter/CalculatePointController.java
@@ -0,0 +1,19 @@
+package interface_adapter;
+
+import entity.Question;
+import use_case.calculate_point.CalculatePointInputBoundary;
+import use_case.calculate_point.CalculatePointInputData;
+import use_case.select_answer.SelectAnswerInputBoundary;
+import use_case.select_answer.SelectAnswerInputData;
+
+public class CalculatePointController {
+    CalculatePointInputBoundary calculatePointInteractor;
+
+    public void calculatePointController(CalculatePointInputBoundary calculatePointInputBoundary) {
+        this.calculatePointInteractor = calculatePointInputBoundary;
+    }
+    public void execute(int answer) {
+        CalculatePointInputData calculatePointInputData = new CalculatePointInputData(answer);
+        calculatePointInteractor.execute(calculatePointInputData);
+    }
+}
