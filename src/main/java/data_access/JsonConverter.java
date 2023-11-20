1684a23693e20f6a48e00e3f82c40ee60b736400
Katia719 <ebrechko2022@gmail.com>
2023-11-20 14:34:38 -0500

    Added Maven and started working on code to convert json string to json object.

---
+++ src/main/java/data_access/JsonConverter.java
@@ -0,0 +1,10 @@
+package data_access;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonConverter {
+    public static void main(String[] args) {
+        // JSON string from ...
+        String jsonString = TriviaDataAccessObject.callApi();
+    }
+}
