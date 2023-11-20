629574150cbd17bb4208d4bb4fca35e2f243c672
ayaan000 <82323240+ayaan000@users.noreply.github.com>
2023-11-20 14:07:07 -0500

    Added button functionalities to Home and Settings GUI and created a placeholder for Game GUI.

--- src/view/HomeGUI.java
+++ src/view/HomeGUI.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class HomeGUI implements ActionListener {
+public class HomeGUI extends JFrame implements ActionListener {
         //Create SettingsGUI
 
         public HomeGUI(){
@@ -19,23 +19,15 @@ public class HomeGUI implements ActionListener {
                         @Override
                         public void actionPerformed(ActionEvent e) {
                             if (e.getSource().equals(singlePlayer)) {
+
                                 SettingsGUI settingsGUI = new SettingsGUI(1);
+                                frame.dispose();
                             }
                         }
                     }
             );
 
-            JButton multiPlayer = new JButton("Multi Player");
-            multiPlayer.addActionListener(
-                    new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            if (e.getSource().equals(multiPlayer)) {
 
-                            }
-                        }
-                    }
-            );
 
             JButton help = new JButton("Help");
             help.addActionListener(
@@ -50,22 +42,25 @@ public class HomeGUI implements ActionListener {
             );
 
 
-            buttonPanel.setBorder(BorderFactory.createEmptyBorder(550, 400, 200, 400));
+            buttonPanel.setBorder(BorderFactory.createEmptyBorder(500, 400, 250, 400));
             buttonPanel.setLayout(new GridLayout(1,3, 100, 100));
             buttonPanel.add(singlePlayer);
-            buttonPanel.add(multiPlayer);
             buttonPanel.add(help);
 
             frame.add(buttonPanel, BorderLayout.CENTER);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setTitle("Task Manager");
-            frame.setSize(500, 500);
+
             frame.pack();
             frame.setVisible(true);
+            frame.setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
+
         }
         public void actionPerformed(ActionEvent evt) {
         System.out.println("Cancel not implemented yet.");
     }
+
+
 
         public static void main(String[] args){
             new HomeGUI();
