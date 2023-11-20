52ed4081fb58d947ad381b04dffd1d6ed5bb5909
Katia719 <ebrechko2022@gmail.com>
2023-11-20 14:52:08 -0500

    made some comments in JsonConverter

--- src/main/java/data_access/JsonConverter.java
+++ src/main/java/data_access/JsonConverter.java
@@ -3,8 +3,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonConverter {
+
     public static void main(String[] args) {
         // JSON string from ...
-        String jsonString = TriviaDataAccessObject.callApi();
+        String jsonString = TriviaDataAccessObject.callApi(); // dont put this in main
     }
 }
