adaf9aa6da8873ee0d1b3d3157bea5e15a5162ae
Katia719 <ebrechko2022@gmail.com>
2023-11-20 22:01:59 -0500

    created template for JsonConverter but there are some things to figure out.
    Notably:
    - where to get inputs for callApi (to make jsonString)
    - if we can get incorrectAnswers as a list directly rather than a String

--- src/main/java/data_access/JsonConverter.java
+++ src/main/java/data_access/JsonConverter.java
@@ -2,10 +2,33 @@ package data_access;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+
 public class JsonConverter {
 
-    public static void main(String[] args) {
-        // JSON string from ...
-        String jsonString = TriviaDataAccessObject.callApi(); // dont put this in main
+    // input from user (use input data or is that backwards?)
+    // still need to define
+    String category;
+    String difficulty;
+    int numberOfQuestions;
+    String jsonString = TriviaDataAccessObject.callApi(category, difficulty, numberOfQuestions);
+
+    public void main(String[] args) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Converting JSON string to JsonNode (generic representation of JSON data)
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+            // Access data from JsonNode
+            String question = jsonNode.get("question").asText();
+            String correctAnswer = jsonNode.get("correct_answer").asText();
+            String incorrectAnswers = jsonNode.get("incorrect_answers").asText();   // for now storing as string
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
