c23c28f0c6f29fa0afc8e91786a6accc7e5e0b23
Katia719 <ebrechko2022@gmail.com>
2023-11-23 00:21:28 -0500

    did a lot of work on JsonConverter
    Notable changes:
    - added a myObject class so that I can use the objectMapper class on it
    - experimented with different ways to get the data from the json file (notably the array of incorrect answers)

--- src/main/java/data_access/JsonConverter.java
+++ src/main/java/data_access/JsonConverter.java
@@ -11,24 +11,99 @@ public class JsonConverter {
     String category;
     String difficulty;
     int numberOfQuestions;
-    String jsonString = TriviaDataAccessObject.callApi(category, difficulty, numberOfQuestions);
+    public JsonConverter (String category, String difficulty, int numberOfQuestions) {
+        this.category = category;
+        this.difficulty = difficulty;
+        this.numberOfQuestions = numberOfQuestions;
 
-    public void main(String[] args) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
+    }
 
-            // Converting JSON string to JsonNode (generic representation of JSON data)
-            JsonNode jsonNode = objectMapper.readTree(jsonString);
+    // OLD VERSION
+//    public void convert(String jsonString) {
+//        try {
+//            ObjectMapper objectMapper = new ObjectMapper();
+//
+//            // Converting JSON string to JsonNode (generic representation of JSON data)
+//            JsonNode rootNode = objectMapper.readTree(jsonString);
+//
+//            // Access data from JsonNode
+//            String question = rootNode.path("results").path("question").asText();
+//            String correctAnswer = rootNode.path("results").path("correct_answer").asText();
+//
+//            //JsonNode incorrectNode = rootNode.path("results").path("incorrect_answers");
+//
+//            //String incorrectAnswers = rootNode.path("results").path("incorrect_answers").asText();
+//            List<String> incorrectAnswers = rootNode.path("results").path("incorrect_answers").readValue();
+//
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+    public void convert(String jsonString) {
+    try {
+        ObjectMapper objectMapper = new ObjectMapper();
 
-            // Access data from JsonNode
-            String question = jsonNode.get("question").asText();
-            String correctAnswer = jsonNode.get("correct_answer").asText();
-            String incorrectAnswers = jsonNode.get("incorrect_answers").asText();   // for now storing as string
+        // read from json file and convert "incorrect_answers" array into String array
+        MyObject myObject = objectMapper.readValue(jsonString, MyObject.class);
 
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        // access data
+        String question = myObject.getQuestion();
+        String correctAnswer = myObject.getCorrectAnswer();
+        String[] incorrectAnswers = myObject.getIncorrectAnswers();
+//        for (String incorrectAnswer : myObject.getIncorrectAnswers()) {
+//
+//        }
+//        int i = 0;
+//        while (i < 3) {
+//            incorrectAnswers[i] = myObject.getIncorrectAnswer
+//            i++;
+//        }
+        System.out.println(question);
+        System.out.println(correctAnswer);
+        System.out.println(incorrectAnswers);
+    }
+    catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+class MyObject {        // temp name
+    private String question;
+    private String correctAnswer;
+    private String[] incorrectAnswers;
+
+    // Getters and setters
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String[] getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(String[] incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+}
+
+    public static void main(String[] args) {
+        TriviaDataAccessObject triviaDataAccessObject = new TriviaDataAccessObject(...);    // csv file?
+        String jsonString = triviaDataAccessObject.callApi("Animals", "medium", 5);
+        JsonConverter.convert(jsonString);  // whyyyyyyy
     }
 
 }
