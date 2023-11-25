371f7cff4210aedd2ddb7eda0869cc0cd65c1a4a
Katia719 <ebrechko2022@gmail.com>
2023-11-25 15:47:00 -0500

    Changed the structure of JsonConverter back to having everything within the convert method (no longer have a subclass). Wrote an Iterator "pattern" to access nested data within the JsonNode.

--- src/main/java/data_access/JsonConverter.java
+++ src/main/java/data_access/JsonConverter.java
@@ -1,104 +1,127 @@
 package data_access;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import entity.Question; // QUESTION: is it right for me to just import the class like this?
 
+import java.util.Iterator;
 import java.util.List;
 
 public class JsonConverter {
 
-    // input from user (use input data or is that backwards?)
+    // QUESTION: input from user (use input data or is that backwards?)
     // still need to define
     String category;
     String difficulty;
     int numberOfQuestions;
+    Question[] data = new Question[numberOfQuestions];
 
-    // OLD VERSION
-//    public void convert(String jsonString) {
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//
-//            // Converting JSON string to JsonNode (generic representation of JSON data)
-//            JsonNode rootNode = objectMapper.readTree(jsonString);
-//
-//            // Access data from JsonNode
-//            String question = rootNode.path("results").path("question").asText();
-//            String correctAnswer = rootNode.path("results").path("correct_answer").asText();
-//
-//            //JsonNode incorrectNode = rootNode.path("results").path("incorrect_answers");
-//
-//            //String incorrectAnswers = rootNode.path("results").path("incorrect_answers").asText();
-//            List<String> incorrectAnswers = rootNode.path("results").path("incorrect_answers").readValue();
-//
-//        }
-//        catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    // ORIGINAL (DIRECT) VERSION
     public void convert(String jsonString) {
-    try {
-        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
 
-        // read from json file and convert "incorrect_answers" array into String array
-        MyObject myObject = objectMapper.readValue(jsonString, MyObject.class);
+            // Converting JSON string to JsonNode (generic representation of JSON data)
+            JsonNode rootNode = objectMapper.readTree(jsonString);
+            // Accessing data from the "results" key
+            JsonNode resultsNode = rootNode.path("results");
 
-        // access data
-        String question = myObject.getQuestion();
-        String correctAnswer = myObject.getCorrectAnswer();
-        String[] incorrectAnswers = myObject.getIncorrectAnswers();
-//        for (String incorrectAnswer : myObject.getIncorrectAnswers()) {
+
+            Iterator<JsonNode> resultsIterator = resultsNode.elements();
+            while (resultsIterator.hasNext()) {
+                JsonNode resultNode = resultsIterator.next();
+                String question = resultNode.path("question").asText();
+                String correctAnswer = resultNode.path("correct_answer").asText();
+
+                // Accessing data from the "incorrect_answers" key
+                JsonNode incorrectsNode = resultNode.path("incorrect_answers");
+                String[] incorrectAnswers = new String[3];
+
+                int i = 0;
+                for (Iterator<JsonNode> it = incorrectsNode.elements(); it.hasNext(); ) {
+                    JsonNode incorrectNode = it.next();
+                    String incorrectAnswer = incorrectNode.asText();
+                    incorrectAnswers[i] = incorrectAnswer;
+                    i++;
+                }
+
+                int j = 0;
+                Question currQuestion = new Question(question, incorrectAnswers, correctAnswer);   // QUESTION: change type in Question?
+                data[j] = currQuestion;
+                j++;    // QUESTION: why does it say that j is never used when it is on line 49??
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // WAY USING SUBCLASS (PROBABLY NOT)
+//    public void convert(String jsonString) {
+//    try {
+//        ObjectMapper objectMapper = new ObjectMapper();
 //
-//        }
-//        int i = 0;
-//        while (i < 3) {
-//            incorrectAnswers[i] = myObject.getIncorrectAnswer
-//            i++;
-//        }
-        System.out.println(question);
-        System.out.println(correctAnswer);
-        System.out.println(incorrectAnswers);
-    }
-    catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-
-class MyObject {        // temp name
-    private String question;
-    private String correctAnswer;
-    private String[] incorrectAnswers;
-
-    // Getters and setters
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getCorrectAnswer() {
-        return correctAnswer;
-    }
-
-    public void setCorrectAnswer(String correctAnswer) {
-        this.correctAnswer = correctAnswer;
-    }
-
-    public String[] getIncorrectAnswers() {
-        return incorrectAnswers;
-    }
-
-    public void setIncorrectAnswers(String[] incorrectAnswers) {
-        this.incorrectAnswers = incorrectAnswers;
-    }
-}
+//        // read from json file and convert "incorrect_answers" array into String array
+//        MyObject myObject = objectMapper.readValue(jsonString, MyObject.class);
+//
+//        // access data
+//        String question = myObject.getQuestion();
+//        String correctAnswer = myObject.getCorrectAnswer();
+//        String[] incorrectAnswers = myObject.getIncorrectAnswers();
+////        for (String incorrectAnswer : myObject.getIncorrectAnswers()) {
+////
+////        }
+////        int i = 0;
+////        while (i < 3) {
+////            incorrectAnswers[i] = myObject.getIncorrectAnswer
+////            i++;
+////        }
+//        System.out.println(question);
+//        System.out.println(correctAnswer);
+//        System.out.println(incorrectAnswers);
+//    }
+//    catch (Exception e) {
+//        e.printStackTrace();
+//    }
+//}
+//
+//class MyObject {        // temp name
+//    private String question;
+//    private String correctAnswer;
+//    private String[] incorrectAnswers;
+//
+//    // Getters and setters
+//
+//    public String getQuestion() {
+//        return question;
+//    }
+//
+//    public void setQuestion(String question) {
+//        this.question = question;
+//    }
+//
+//    public String getCorrectAnswer() {
+//        return correctAnswer;
+//    }
+//
+//    public void setCorrectAnswer(String correctAnswer) {
+//        this.correctAnswer = correctAnswer;
+//    }
+//
+//    public String[] getIncorrectAnswers() {
+//        return incorrectAnswers;
+//    }
+//
+//    public void setIncorrectAnswers(String[] incorrectAnswers) {
+//        this.incorrectAnswers = incorrectAnswers;
+//    }
+//}
 
     public static void main(String[] args) {
-        TriviaDataAccessObject triviaDataAccessObject = new TriviaDataAccessObject();    // csv file?
+        TriviaDataAccessObject triviaDataAccessObject = new TriviaDataAccessObject();
         String jsonString = triviaDataAccessObject.callApi("Animals", "medium", 5);
         JsonConverter myObj = new JsonConverter();
-        myObj.convert(jsonString);  // whyyyyyyy
+        myObj.convert(jsonString);
     }
 
 }
