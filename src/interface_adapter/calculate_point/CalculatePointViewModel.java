bcc57a1f07fbb9956328bbff8c86934d9c3da489
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-27 13:16:19 -0500

    calculate point view model without state

--- src/interface_adapter/calculate_point/CalculatePointViewModel.java
+++ src/interface_adapter/calculate_point/CalculatePointViewModel.java
@@ -1,2 +1,35 @@
-package interface_adapter.calculate_point;public class CalculatePointViewModel {
+package interface_adapter.calculate_point;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class CalculatePointViewModel {
+    public static final String TITLE_LABEL = "Total Point";
+    public static final String PLAYER_TITLE_LABEL = "Player";
+    private CalculatePointState state = new CalculatePointState();
+
+    public CalculatePointViewModel() {
+        super("calculate point");
+    }
+
+    public void setState(CalculatePointState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public CalculatePointState getState() {
+        return state;
+    }
 }
+
