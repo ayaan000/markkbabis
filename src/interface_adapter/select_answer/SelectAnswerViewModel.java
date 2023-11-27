f1a97bedae1e543a7b387d0d67ca21931fc9926b
khoibui105 <buiminhkhoi105@gmail.com>
2023-11-27 13:03:50 -0500

    select answer view model without state

--- src/interface_adapter/select_answer/SelectAnswerViewModel.java
+++ src/interface_adapter/select_answer/SelectAnswerViewModel.java
@@ -1,2 +1,39 @@
-package interface_adapter.select_answer;public class SelectAnswerViewModel {
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class SelectAnswerViewModel extends ViewModel {
+    public static final String TITLE_LABEL = "Question";
+    public static final String A_BUTTON_LABEL = "A";
+    public static final String B_BUTTON_LABEL = "B";
+    public static final String C_BUTTON_LABEL = "C";
+    public static final String D_BUTTON_LABEL = "D";
+
+    private SelectAnswerState state = new SelectAnswerState();
+
+    public SelectAnswerViewModel() {
+        super("select answer");
+    }
+
+    public void setState(SelectAnswerState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public SelectAnswerState getState() {
+        return state;
+    }
 }
