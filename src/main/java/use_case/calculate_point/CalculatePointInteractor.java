df25d4e23ad6b989649577173d148b5000efd654
khoibui105 <buiminhkhoi105@gmail.com>
2023-12-02 23:29:33 -0500

    calculate point interactor and edit select answer output boundary

--- src/main/java/use_case/calculate_point/CalculatePointInteractor.java
+++ src/main/java/use_case/calculate_point/CalculatePointInteractor.java
@@ -1,2 +1,17 @@
-package use_case.calculate_point;public class CalculatePointInteractor {
+package use_case.calculate_point;
+
+import java.time.Duration;
+
+public class CalculatePointInteractor implements CalculatePointInputBoundary{
+    @Override
+    public void execute(CalculatePointInputData calculatePointInputData) {
+        boolean correctness = calculatePointInputData.getCorrectness();
+        Duration timeLeft = calculatePointInputData.getTimeLeft();
+        if (correctness) {
+            // player.point = player.point + 100 + round(10 * timeLeft);
+        }
+        else {
+            // player.point += 0;
+        }
+    }
 }
