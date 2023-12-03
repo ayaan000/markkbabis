186c4783d838a6975e34aa44ad00dd02fc32341f
ayaan000 <82323240+ayaan000@users.noreply.github.com>
2023-12-03 16:52:47 -0500

    Working on Game Manager

--- src/main/java/app/GameManager.java
+++ src/main/java/app/GameManager.java
@@ -1,26 +1,25 @@
 package app;
-import data_access.TriviaDataAccessObject;
-import entity.*;
-import interface_adapter.ViewManagerModel;
-import view.GameGUI;
-import view.HomeGUI;
-import view.SettingsGUI;
 
-import javax.swing.*;
-import java.awt.*;
+import data_access.TriviaDataAccessObject;
+import interface_adapter.initialize_game.InitializeGameController;
+import interface_adapter.initialize_game.InitializeGamePresenter;
+import use_case.initialize_game.InitializeGameInputBoundary;
+import use_case.initialize_game.InitializeGameInteractor;
+import use_case.initialize_game.InitializeGameOutputBoundary;
+import view.HomeGUI;
 
 public class GameManager {
 
-    public Question[] initialize_game(){
-
-    }
 
     public static void main(String[] args) {
-
-
-        HomeGUI homeViewModel = new HomeGUI();
-
         TriviaDataAccessObject userDataAccessObject = new TriviaDataAccessObject();
+
+        InitializeGameOutputBoundary initializeGameOutputBoundary = new InitializeGamePresenter();
+        InitializeGameInputBoundary initializeGameInteractor = new InitializeGameInteractor(userDataAccessObject, initializeGameOutputBoundary);
+        InitializeGameController initializeGameController = new InitializeGameController(initializeGameInteractor);
+
+
+
 
 
     }
