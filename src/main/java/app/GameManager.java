b515d27510696cdabd0c0e3e06b38026fc9dfcb9
ayaan000 <82323240+ayaan000@users.noreply.github.com>
2023-12-03 22:59:26 -0500

    Passed InitializeGameController to SettingsGUI and created GameState and added to GameManager

--- src/main/java/app/GameManager.java
+++ src/main/java/app/GameManager.java
@@ -4,7 +4,6 @@ import data_access.TriviaDataAccessObject;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.initialize_game.InitializeGameController;
 import interface_adapter.initialize_game.InitializeGamePresenter;
-import interface_adapter.initialize_game.InitializeGameViewModel;
 import use_case.initialize_game.InitializeGameInputBoundary;
 import use_case.initialize_game.InitializeGameInteractor;
 import use_case.initialize_game.InitializeGameOutputBoundary;
@@ -15,13 +14,12 @@ public class GameManager {
 
     public static void main(String[] args) {
         TriviaDataAccessObject userDataAccessObject = new TriviaDataAccessObject();
-        InitializeGameViewModel initializeGameViewModel = new InitializeGameViewModel();
+//        InitializeGameViewModel initializeGameViewModel = new InitializeGameViewModel();
         ViewManagerModel viewManagerModel = new ViewManagerModel();
-        InitializeGameOutputBoundary initializeGameOutputBoundary = new InitializeGamePresenter(initializeGameViewModel, viewManagerModel);
+        InitializeGameOutputBoundary initializeGameOutputBoundary = new InitializeGamePresenter();
         InitializeGameInputBoundary initializeGameInteractor = new InitializeGameInteractor(userDataAccessObject, initializeGameOutputBoundary);
         InitializeGameController initializeGameController = new InitializeGameController(initializeGameInteractor);
         new HomeGUI(initializeGameController);
-
 
 
     }
