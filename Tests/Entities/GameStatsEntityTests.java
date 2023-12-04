9c194359daed695fd73f30fbe84ff5e388438044
Inkim1 <matthew.inkim@mail.utoronto.ca>
2023-12-04 13:40:45 -0500

    Fixing GameStatsEntityTests

--- Tests/Entities/GameStatsEntityTests.java
+++ Tests/Entities/GameStatsEntityTests.java
@@ -25,6 +25,7 @@ public class GameStatsEntityTests {
         Game game = new Game("Animals", question, questions);
         GameStats gameStats = new GameStats(game, questions, "Medium", 1, 0,
                 0, 0, 0);
+
         assert (Objects.equals(gameStats.getGame().getCurrCategory(), "Animals") &&
                 Objects.equals(gameStats.getGame().getCurrQuestion().getQuestion(),
                         "What is the collective noun for bears?") &&
@@ -36,7 +37,39 @@ public class GameStatsEntityTests {
                 gameStats.getPlayerPoints() == 0 &&
                 gameStats.getComputerPoints() == 0 &&
                 gameStats.getNumCorrectAnswers1() == 0 &&
-                gameStats.getNumCorrectAnswers2() == 0);
+                gameStats.getNumCorrectAnswers2() == 0 &&
+                gameStats.getGame().getQuestionList() == gameStats.getQuestions());
+    }
 
+    @Test
+    public void RunGameStats1() {
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Sloth");
+        List<Byte> avatar = new ArrayList<Byte>();
+        ArrayList<Question> questions = new ArrayList<Question>();
+        questions.add(question);
+        Game game = new Game("Animals", question, questions);
+        GameStats gameStats = new GameStats(game, questions, "Medium", 1, 10,
+                20, 10, 20);
+
+        assert (Objects.equals(gameStats.getGame().getCurrCategory(), "Animals") &&
+                Objects.equals(gameStats.getGame().getCurrQuestion().getQuestion(),
+                        "What is the collective noun for bears?") &&
+                gameStats.getGame().getQuestionList().equals(questions) &&
+
+                gameStats.getQuestions()== questions &&
+                Objects.equals(gameStats.getDifficulty(), "Medium") &&
+                gameStats.getNumQuestions() == 1 &&
+                gameStats.getGame().getQuestionList() == gameStats.getQuestions() &&
+
+                gameStats.getPlayerPoints() == 10 &&
+                gameStats.getComputerPoints() == 20 &&
+                gameStats.getNumCorrectAnswers1() == 10 &&
+                gameStats.getNumCorrectAnswers2() == 20);
     }
 }
