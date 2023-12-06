3c2b1ee74c1f63c69cec2f0eefc5de30842012ea
Inkim1 <matthew.inkim@mail.utoronto.ca>
2023-12-06 17:14:55 -0500

    Finished Computer Entity Tests to work

--- Tests/Entities/GameStatsEntityTests.java
+++ Tests/Entities/GameStatsEntityTests.java
@@ -1,9 +1,6 @@
 package Entities;
 
-import entity.Game;
-import entity.GameStats;
-import entity.Player;
-import entity.Question;
+import entity.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,7 +9,9 @@ import java.util.Objects;
 
 public class GameStatsEntityTests {
     @Test
-    public void InitialiseGameStats() {
+    public void InitialiseGameStats1() {
+
+        List<Byte> avatar = new ArrayList<Byte>();
 
         List<String> possibleAnswer = new ArrayList<String>();
         possibleAnswer.add("Sloth");
@@ -21,19 +20,88 @@ public class GameStatsEntityTests {
         possibleAnswer.add("Husk");
         Question question = new Question("What is the collective noun for bears?",
                 possibleAnswer, "Sloth");
-        ArrayList<Question> questions = new ArrayList<Question>();
-        questions.add(question);
+        Question[] questions = new Question[1];
+        questions[0] = question;
+        Game game = new Game(questions);
+
+        List<GameStats> gameStatsList = new ArrayList<GameStats>();
+
+        Player player = new Player(0, avatar, new ArrayList<GameStats>(), game);
+        Computer computer = new Computer("easy");
+
+        int playerPoints = player.getTotalPoints();
+        int computerPoints = computer.getTotalPoints2();
+
+        GameStats gameStats = new GameStats(playerPoints, computerPoints);
+
+        assert (gameStats.getPlayerPoints() == 0 &&
+                gameStats.getComputerPoints() == 0);
+    }
+
+    @Test
+    public void InitialiseGameStats2() {
 
         List<Byte> avatar = new ArrayList<Byte>();
 
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Sloth");
+        Question[] questions = new Question[1];
+        questions[0] = question;
         Game game = new Game(questions);
 
-        Player player = Player(0, new ArrayList<Byte>(), );
-        int playerPoints = ;
-        GameStats gameStats = new GameStats()
+        List<GameStats> gameStatsList = new ArrayList<GameStats>();
 
-        assert (gameStats.getPlayerPoints() ==   &&
-                gameStats.getComputerPoints() == );
+        Player player = new Player(0, avatar, new ArrayList<GameStats>(), game);
+        Computer computer = new Computer("easy");
+
+        player.setAdditionalPoints(25);
+        computer.setAdditionPoints(50);
+
+        int playerPoints = player.getTotalPoints();
+        int computerPoints = computer.getTotalPoints2();
+
+        GameStats gameStats = new GameStats(playerPoints, computerPoints);
+
+        assert (gameStats.getPlayerPoints() == 25 &&
+                gameStats.getComputerPoints() == 50);
+    }
+
+    @Test
+    public void InitialiseGameStats3() {
+
+        List<Byte> avatar = new ArrayList<Byte>();
+
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Sloth");
+        Question[] questions = new Question[1];
+        questions[0] = question;
+        Game game = new Game(questions);
+
+        List<GameStats> gameStatsList = new ArrayList<GameStats>();
+
+        Player player = new Player(0, avatar, new ArrayList<GameStats>(), game);
+        Computer computer = new Computer("easy");
+
+        player.setAdditionalPoints(0);
+        computer.setAdditionPoints(500);
+
+        int playerPoints = player.getTotalPoints();
+        int computerPoints = computer.getTotalPoints2();
+
+        GameStats gameStats = new GameStats(playerPoints, computerPoints);
+
+        assert (gameStats.getPlayerPoints() == 0 &&
+                gameStats.getComputerPoints() == 500);
     }
 
 //    @Test
--- Tests/Entities/QuestionEntityTests.java
+++ Tests/Entities/QuestionEntityTests.java
@@ -1,119 +1,119 @@
-//package Entities;
-//
-//import entity.Game;
-//import entity.GameStats;
-//import entity.Player;
-//import entity.Question;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class QuestionEntityTests {
-//    @Before
-//    public void init(){
-//        List<String> possibleAnswer = new ArrayList<String>();
-//        possibleAnswer.add("Sloth");
-//        possibleAnswer.add("Drove");
-//        possibleAnswer.add("Tribe");
-//        possibleAnswer.add("Husk");
-//        Question question = new Question("What is the collective noun for bears?",
-//                possibleAnswer, "Sloth" );
-//    }
-//    @Test
-//    public void testGetCorrectAnswer() {
-//        List<String> possibleAnswer = new ArrayList<String>();
-//        possibleAnswer.add("Sloth");
-//        possibleAnswer.add("Drove");
-//        possibleAnswer.add("Tribe");
-//        possibleAnswer.add("Husk");
-//        Question question = new Question("What is the collective noun for bears?",
-//                possibleAnswer, "Sloth" );
-//        assertEquals("Sloth", question.getCorrectAnswer());
-//    }
-//    @Test
-//    public void testSetCorrectAnswer() {
-//        List<String> possibleAnswer = new ArrayList<String>();
-//        possibleAnswer.add("Sloth");
-//        possibleAnswer.add("Drove");
-//        possibleAnswer.add("Tribe");
-//        possibleAnswer.add("Husk");
-//        Question question = new Question("What is the collective noun for bears?",
-//                possibleAnswer, "Tribe" );
-//        question.setCorrectAnswer("Sloth");
-//        assertEquals("Sloth", question.getCorrectAnswer());
-//    }
-//    @Test
-//    public void testGetIndexAnswer() {
-//        List<String> possibleAnswer = new ArrayList<String>();
-//        possibleAnswer.add("Sloth");
-//        possibleAnswer.add("Drove");
-//        possibleAnswer.add("Tribe");
-//        possibleAnswer.add("Husk");
-//        Question question = new Question("What is the collective noun for bears?",
-//                possibleAnswer, "Sloth" );
-//        assertEquals(0, possibleAnswer.indexOf("Sloth"));
-//    }
-//    @Test
-//    public void testGetPossibleAnswer() {
-//        List<String> possibleAnswer = new ArrayList<String>();
-//        Question question = new Question("What is the collective noun for bears?",
-//                possibleAnswer, "Sloth" );
-//        List<String> possibleAnswer1 = new ArrayList<String>();
-//        possibleAnswer1.add("Sloth");
-//        possibleAnswer1.add("Drove");
-//        possibleAnswer1.add("Tribe");
-//        possibleAnswer1.add("Husk");
-//        question.setPossibleAnswer(possibleAnswer1);
-//        List<String> possibleAnswer2 = new ArrayList<String>();
-//        possibleAnswer2.add("Sloth");
-//        possibleAnswer2.add("Drove");
-//        possibleAnswer2.add("Tribe");
-//        possibleAnswer2.add("Husk");
-//        question.setPossibleAnswer(possibleAnswer1);
-//        assertEquals(possibleAnswer2, question.getPossibleAnswer());
-//    }
-//    @Test
-//    public void testSetPossibleAnswer() {
-//        Question question = new Question("What is the collective noun for bears?",
-//                new ArrayList<String>(), "Sloth" );
-//        List<String> possibleAnswer1 = new ArrayList<String>();
-//        possibleAnswer1.add("Sloth");
-//        possibleAnswer1.add("Drove");
-//        possibleAnswer1.add("Tribe");
-//        possibleAnswer1.add("Husk");
-//        question.setPossibleAnswer(possibleAnswer1);
-//        List<String> listPossibleAnswer = Arrays.asList("Sloth", "Drove", "Tribe", "Husk");
-//        question.setPossibleAnswer(possibleAnswer1);
-//        assertEquals(listPossibleAnswer, question.getPossibleAnswer());
-//    }
-//    @Test
-//    public void testGetQuestion() {
-//        List<String> possibleAnswer = new ArrayList<String>();
-//        possibleAnswer.add("Sloth");
-//        possibleAnswer.add("Drove");
-//        possibleAnswer.add("Tribe");
-//        possibleAnswer.add("Husk");
-//        String ques = "What is the collective noun for bears?";
-//        Question question = new Question("", possibleAnswer, "Sloth" );
-//        question.setQuestion(ques);
-//        assertEquals("What is the collective noun for bears?", question.getQuestion());
-//    }
-//    @Test
-//    public void testSetQuestion() {
-//        List<String> possibleAnswer = new ArrayList<String>();
-//        possibleAnswer.add("Sloth");
-//        possibleAnswer.add("Drove");
-//        possibleAnswer.add("Tribe");
-//        possibleAnswer.add("Husk");
-//        String ques = "What is the collective noun for bears?";
-//        Question question = new Question("", possibleAnswer, "Sloth" );
-//        question.setQuestion(ques);
-//        assertEquals("What is the collective noun for bears?", question.getQuestion());
-//    }
-//
-//}
+package Entities;
+
+import entity.Game;
+import entity.GameStats;
+import entity.Player;
+import entity.Question;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class QuestionEntityTests {
+    @Before
+    public void init(){
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Sloth" );
+    }
+    @Test
+    public void testGetCorrectAnswer() {
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Sloth" );
+        assertEquals("Sloth", question.getCorrectAnswer());
+    }
+    @Test
+    public void testSetCorrectAnswer() {
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Tribe" );
+        question.setCorrectAnswer("Sloth");
+        assertEquals("Sloth", question.getCorrectAnswer());
+    }
+    @Test
+    public void testGetIndexAnswer() {
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Sloth" );
+        assertEquals(0, possibleAnswer.indexOf("Sloth"));
+    }
+    @Test
+    public void testGetPossibleAnswer() {
+        List<String> possibleAnswer = new ArrayList<String>();
+        Question question = new Question("What is the collective noun for bears?",
+                possibleAnswer, "Sloth" );
+        List<String> possibleAnswer1 = new ArrayList<String>();
+        possibleAnswer1.add("Sloth");
+        possibleAnswer1.add("Drove");
+        possibleAnswer1.add("Tribe");
+        possibleAnswer1.add("Husk");
+        question.setPossibleAnswer(possibleAnswer1);
+        List<String> possibleAnswer2 = new ArrayList<String>();
+        possibleAnswer2.add("Sloth");
+        possibleAnswer2.add("Drove");
+        possibleAnswer2.add("Tribe");
+        possibleAnswer2.add("Husk");
+        question.setPossibleAnswer(possibleAnswer1);
+        assertEquals(possibleAnswer2, question.getPossibleAnswer());
+    }
+    @Test
+    public void testSetPossibleAnswer() {
+        Question question = new Question("What is the collective noun for bears?",
+                new ArrayList<String>(), "Sloth" );
+        List<String> possibleAnswer1 = new ArrayList<String>();
+        possibleAnswer1.add("Sloth");
+        possibleAnswer1.add("Drove");
+        possibleAnswer1.add("Tribe");
+        possibleAnswer1.add("Husk");
+        question.setPossibleAnswer(possibleAnswer1);
+        List<String> listPossibleAnswer = Arrays.asList("Sloth", "Drove", "Tribe", "Husk");
+        question.setPossibleAnswer(possibleAnswer1);
+        assertEquals(listPossibleAnswer, question.getPossibleAnswer());
+    }
+    @Test
+    public void testGetQuestion() {
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        String ques = "What is the collective noun for bears?";
+        Question question = new Question("", possibleAnswer, "Sloth" );
+        question.setQuestion(ques);
+        assertEquals("What is the collective noun for bears?", question.getQuestion());
+    }
+    @Test
+    public void testSetQuestion() {
+        List<String> possibleAnswer = new ArrayList<String>();
+        possibleAnswer.add("Sloth");
+        possibleAnswer.add("Drove");
+        possibleAnswer.add("Tribe");
+        possibleAnswer.add("Husk");
+        String ques = "What is the collective noun for bears?";
+        Question question = new Question("", possibleAnswer, "Sloth" );
+        question.setQuestion(ques);
+        assertEquals("What is the collective noun for bears?", question.getQuestion());
+    }
+
+}
